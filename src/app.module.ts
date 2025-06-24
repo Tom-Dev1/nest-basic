8f989cd9da2a487824f95c61b820cfbf5d089db3
Thanh Tong <truongthanhh88@gmail.com>
2025-06-24 20:21:25 +0700

    feat: add @nestjs/config for environment configuration and update MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://truongthanhh88:ru9RacQX7eV2rCwn@cluster0.ldolsax.mongodb.net/')],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://truongthanhh88:ru9RacQX7eV2rCwn@cluster0.ldolsax.mongodb.net/'),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
